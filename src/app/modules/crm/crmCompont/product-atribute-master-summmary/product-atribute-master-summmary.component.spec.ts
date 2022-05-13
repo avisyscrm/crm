42ee5ed68b87fb546b3938b006fa4ca04dbe79af
santosh zanak <santoshzanak@santoshs-MacBook-Pro.local>
2022-05-13 20:39:42 +0530

    chnage

--- src/app/modules/crm/crmCompont/product-atribute-master-summmary/product-atribute-master-summmary.component.spec.ts
+++ src/app/modules/crm/crmCompont/product-atribute-master-summmary/product-atribute-master-summmary.component.spec.ts
@@ -1,25 +1,5 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ProductAtributeMasterSummmaryComponent } from './product-atribute-master-summmary.component';
 
-describe('ProductAtributeMasterSummmaryComponent', () => {
-  let component: ProductAtributeMasterSummmaryComponent;
-  let fixture: ComponentFixture<ProductAtributeMasterSummmaryComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ProductAtributeMasterSummmaryComponent ]
-    })
-    .compileComponents();
-  }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ProductAtributeMasterSummmaryComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
